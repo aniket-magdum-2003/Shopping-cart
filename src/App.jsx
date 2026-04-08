d7a0c12d11438b406c19b777ecc66b6ebb2353f3
aniket-magdum-2003 <magdumaniket42@gmail.com>
2026-04-08 16:33:12 +0530

    ShoppingCart Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import CartItem from './components/CartItem'
 import { useCart } from './context/cartContext.jsx'
+import ShoppingCart from './components/ShoppingCart.jsx'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
       <h1 className='text-5xl italic text-gray-500 mb-16'>
         Trend Alert : Must-Have Outfits of the Season
       </h1>
+      <ShoppingCart/>
 
       <div className='grid grid-cols-3 place-items-start gap-10'>
         {allItems?.map((item) => (
